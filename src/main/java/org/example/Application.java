33ac98e889c3ec02e4ed6b054498e8df19b121e4
Federico Poggi <federicopoggi3@gmail.com>
2023-12-18 15:09:34 +0100

    Aggiunta Classi e Package Generali

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,8 +1,15 @@
 package org.example;
 
-public class Application {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+public class Application {
+    public static final EntityManagerFactory managerFactory= Persistence.createEntityManagerFactory("bw1812");
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManager entityManager= managerFactory.createEntityManager();
+
+        entityManager.close();
+        managerFactory.close();
     }
 }
---
+++ src/main/java/org/example/EntitiesDAO/MezzoDAO.java
@@ -0,0 +1,4 @@
+package org.example.EntitiesDAO;
+
+public class MezzoDAO {
+}
