3695ea8007720eb8a977f1379c261173d3572772
Federico Poggi <federicopoggi3@gmail.com>
2023-12-18 15:35:46 +0100

    inizializzata classe user e card

--- src/main/java/org/example/Entities/Card.java
+++ src/main/java/org/example/Entities/Card.java
@@ -1,4 +1,66 @@
 package org.example.Entities;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table (name = "card")
 public class Card {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column (name = "card_id")
+    private Long id;
+    @Column (name = "card_number", unique = true, nullable = false, length = 16)
+    private String cardNumber;
+
+    @Column (name = "issue_date", nullable = false, length = 10)
+    private LocalDate issueDate;
+    @Column (name = "stop_date", nullable = false, length = 10)
+    private LocalDate stopDate; //Di default +1 anno dalla data di rilascio
+    @OneToOne (fetch = FetchType.EAGER)
+    private User user;
+
+
+    /*----------------------< Costruttori >---------------------------*/
+
+    public Card() {
+    }
+
+    public Card(Long id, String cardNumber, LocalDate issueDate, LocalDate stopDate, User user) {
+        this.id = id;
+        this.cardNumber = cardNumber;
+        this.issueDate = issueDate;
+        this.stopDate = stopDate;
+        this.user = user;
+    }
+
+    //DA VERIFICARE SE EFFETIVAMENTE SERVE UN COSTRUTTORE SOLO CON LA CARD
+    // VERIFICA FATTA DOPO L'IMPLEMENTAZIONE DEI METODI
+    public Card(User user) {
+        this.user = user;
+    }
+
+    /*--------------------< Getter and Setter >--------------------------*/
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public LocalDate getIssueDate() {
+        return issueDate;
+    }
+
+    public LocalDate getStopDate() {
+        return stopDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    /*---------------------------< Metodi >-----------------------------*/
 }
--- src/main/java/org/example/Entities/User.java
+++ src/main/java/org/example/Entities/User.java
@@ -1,4 +1,77 @@
 package org.example.Entities;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity
+@Table (name = "users")
 public class User {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column (name = "user_name", nullable = false, insertable = true, length = 30)
+    private String userName;
+
+    @Column (name = "user_last_name", nullable = false, insertable = true, length = 30)
+    private String userLastName;
+
+    @Column (name = "birth_date", nullable = false, insertable = true, length = 10)
+    LocalDate birthDate;
+
+    @Column (name = "residential_address", nullable = false, insertable = true, length = 100)
+    private String residentialAddress;
+
+    //FETCH TYPE EAGER PERCHE VOGLIO RECUPERARE ANCHE I DATI RELATIVI ALLA TESSERE UTENTE
+    @OneToOne (fetch = FetchType.EAGER)
+    private Card card;
+
+    /*----------------------< Costruttori >---------------------------*/
+    public User() {
+    }
+
+    public User(Long userId, String userName, String userLastName, LocalDate birthDate, String residentialAddress, Card card) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userLastName = userLastName;
+        this.birthDate = birthDate;
+        this.residentialAddress = residentialAddress;
+        this.card = card;
+    }
+
+    //DA VERIFICARE SE EFFETIVAMENTE SERVE UN COSTRUTTORE SOLO CON LA CARD
+    // VERIFICA FATTA DOPO L'IMPLEMENTAZIONE DEI METODI
+    public User(Card card) {
+        this.card = card;
+    }
+
+    /*--------------------< Getter and Setter >--------------------------*/
+    //--> GETTER
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserLastName() {
+        return userLastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public String getResidentialAddress() {
+        return residentialAddress;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+
+    /*---------------------------< Metodi >-----------------------------*/
 }
