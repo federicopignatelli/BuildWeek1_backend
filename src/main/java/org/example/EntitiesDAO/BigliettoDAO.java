4e8102f4dfc31c4d4177cc9587badb72da7e82cf
Federico Poggi <federicopoggi3@gmail.com>
2023-12-19 15:49:27 +0100

    DAO

--- src/main/java/org/example/EntitiesDAO/BigliettoDAO.java
+++ src/main/java/org/example/EntitiesDAO/BigliettoDAO.java
@@ -1,4 +1,19 @@
 package org.example.EntitiesDAO;
 
+import org.example.Entities.Biglietto;
+
+import javax.persistence.EntityManager;
+
 public class BigliettoDAO {
+
+    EntityManager em;
+
+    public BigliettoDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public void create(Biglietto bi){}
+
+    public void delete(Biglietto bi){}
+
 }
--- src/main/java/org/example/EntitiesDAO/CardDAO.java
+++ src/main/java/org/example/EntitiesDAO/CardDAO.java
@@ -1,4 +1,13 @@
 package org.example.EntitiesDAO;
 
+import org.example.Entities.Card;
+
+import javax.persistence.EntityManager;
+
 public class CardDAO {
+    EntityManager em;
+    public void create(Card ca){}
+    public void delete(Card ca){}
+    
+
 }
