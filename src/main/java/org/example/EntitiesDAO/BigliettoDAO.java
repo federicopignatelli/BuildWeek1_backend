aa2ff7644a042373a9883d2c45dd9200f8e23a94
Riccardo Di Bari <riccardodibari@hotmail.it>
2023-12-19 16:36:48 +0100

    prova di funzione

--- src/main/java/org/example/EntitiesDAO/BigliettoDAO.java
+++ src/main/java/org/example/EntitiesDAO/BigliettoDAO.java
@@ -3,6 +3,9 @@ package org.example.EntitiesDAO;
 import org.example.Entities.Biglietto;
 
 import javax.persistence.EntityManager;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BigliettoDAO {
 
@@ -20,5 +23,43 @@ public class BigliettoDAO {
     }
 
     public void delete(Biglietto bi){}
+
+
+    class Biglietto {
+        private LocalDate dataVidimazione;
+
+        public Biglietto(LocalDate dataVidimazione) {
+            this.dataVidimazione = dataVidimazione;
+        }
+
+        public LocalDate getDataVidimazione() {
+            return dataVidimazione;
+        }
+    }
+
+    class MezzoDiTrasporto {
+        private String nome;
+        private List<Biglietto> biglietti;
+
+        public MezzoDiTrasporto(String nome) {
+            this.nome = nome;
+            this.biglietti = new ArrayList<>();
+        }
+
+        public void aggiungiBiglietto(Biglietto biglietto) {
+            biglietti.add(biglietto);
+        }
+
+        public int contaBigliettiVidimati(LocalDate inizioPeriodo, LocalDate finePeriodo) {
+            int conteggio = 0;
+            for (Biglietto biglietto : biglietti) {
+                LocalDate dataVidimazione = biglietto.getDataVidimazione();
+                if (dataVidimazione.isEqual(inizioPeriodo) || (dataVidimazione.isAfter(inizioPeriodo) && dataVidimazione.isBefore(finePeriodo))) {
+                    conteggio++;
+                }
+            }
+            return conteggio;
+        }
+    }
 
 }
