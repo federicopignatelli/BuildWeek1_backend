33d978e46bcf884840c1ed54b20178c540980dbe
Federico Poggi <federicopoggi3@gmail.com>
2023-12-19 15:42:44 +0100

    Attribuzioni relazioni

--- src/main/java/org/example/EntitiesDAO/AbbonamentoDAO.java
+++ src/main/java/org/example/EntitiesDAO/AbbonamentoDAO.java
@@ -1,4 +1,14 @@
 package org.example.EntitiesDAO;
 
+import org.example.Entities.Abbonamento;
+
+import javax.persistence.EntityManager;
+
 public class AbbonamentoDAO {
-}
+    EntityManager em;
+
+    public AbbonamentoDAO(EntityManager em){
+        this.em = em;
+    }
+    public void create(Abbonamento abbonamento) {};
+    public void delete(Abbonamento abbonamento) {};
