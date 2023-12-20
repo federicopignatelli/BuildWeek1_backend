6abd8afb68d81fefcfbd04c4c4f883f52ad3243a
federicopoggi <138136326+federicopoggi@users.noreply.github.com>
2023-12-20 15:30:17 +0100

    Aggiunto logback

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -5,28 +5,33 @@ import org.example.Entities.Distributore;
 import org.example.Entities.Distributorefisico;
 import org.example.EntitiesDAO.BigliettoDAO;
 import org.example.EntitiesDAO.DistributoreDAO;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.time.LocalDate;
+
 
 public class Application {
     public static final EntityManagerFactory managerFactory = Persistence.createEntityManagerFactory("bw1812");
-
+    private static Logger logger= LoggerFactory.getLogger(Application.class);
     public static void main(String[] args) {
         EntityManager entityManager = managerFactory.createEntityManager();
         // Distributorefisico ds= new Distributorefisico("Padova", "FISICO", 30, 34);
-        DistributoreDAO dm = new DistributoreDAO(entityManager);
+        /*DistributoreDAO dm = new DistributoreDAO(entityManager);
         BigliettoDAO bid = new BigliettoDAO(entityManager);
 
-        /*Distributore ds = new Distributorefisico("Padova", "FISICO", 0, 34, "Gimmy S.N.C");*/
+        *//*Distributore ds = new Distributorefisico("Padova", "FISICO", 0, 34, "Gimmy S.N.C");*//*
 
         Distributore da = new Distributorefisico("Milano", "FISICO", 0, 0, "Biglietteria al crepuscolo' s.n.c");
         dm.save(da);
         Biglietto biglietto = new Biglietto("SESSANTAMINUTI", 1.70);
         bid.create(biglietto, da);
-        System.out.println(da.toString());
+        System.out.println(da.toString());*/
+        /*logger.info("Ciao");*/
+
 
         entityManager.close();
         managerFactory.close();
